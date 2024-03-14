bb7397c465e53a7f41665df4ed2a064a5cfe710c
yuhang0000 <104742794+yuhang0000@users.noreply.github.com>
2024-03-14 17:23:34 +0800

    Add files via upload

---
+++ Repos/Display LightPoint Fixer/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Display_LightPoint_Fixer
+{
+    partial class win1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(12, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 57);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "获取分辨率";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "这是文本";
+            this.label1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(586, 381);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(202, 57);
+            this.button2.TabIndex = 0;
+            this.button2.TabStop = false;
+            this.button2.Text = "我走了哈，我不打扰。";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(183, 381);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 57);
+            this.button3.TabIndex = 0;
+            this.button3.TabStop = false;
+            this.button3.Text = "直接干";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1920, 1080);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Enabled = false;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(354, 381);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 57);
+            this.button4.TabIndex = 0;
+            this.button4.TabStop = false;
+            this.button4.Text = "鼠标穿透";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // win1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Enabled = false;
+            this.KeyPreview = true;
+            this.Name = "win1";
+            this.ShowInTaskbar = false;
+            this.Text = "Display LightPoint Fixer（屏幕白斑，我草泥马） by:yuhang0000";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.win1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Repos/Display LightPoint Fixer/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Display_LightPoint_Fixer
+{
+    public partial class win1 : Form
+    {
+        public win1()
+        {
+            InitializeComponent();
+            SetWindowLong(this.Handle, GWL_EXSTYLE, WS_EX_LAYERED);
+            SetLayeredWindowAttributes(this.Handle, 255, 255, LWA_COLORKEY);
+            TransparencyKey = BackColor;//背景透明(鼠标穿透)
+        }
+        private const uint WS_EX_LAYERED = 0x80000;
+        private const int GWL_EXSTYLE = -20;
+        private const int LWA_COLORKEY = 1;
+
+        [DllImport("user32", EntryPoint = "SetWindowLong")]
+        private static extern uint SetWindowLong(IntPtr hwnd, int nIndex, uint dwNewLong);
+
+        [DllImport("user32", EntryPoint = "SetLayeredWindowAttributes")]
+        private static extern int SetLayeredWindowAttributes(IntPtr hwnd, int crKey, int bAlpha, int dwFlags);
+
+        
+        //程序启动时自动运行代码
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int xWidth1 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width;//获取显示器屏幕宽度
+            int yHeight1 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height;//高度
+            //Console.WriteLine(xWidth1 + "px * " + yHeight1 + "px");
+            this.Width = xWidth1;
+            this.Height = yHeight1;
+            this.Left = 0;
+            this.Top = 0;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true; this.ResumeLayout(false);
+            int xWidth2 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width;
+            int yHeight2 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height;
+            this.Width = xWidth2;
+            this.Height = yHeight2;
+            this.ResumeLayout(false);
+            OpenFileDialog open = new OpenFileDialog();
+            DialogResult result = open.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.pictureBox1.Image = Image.FromFile(open.FileName);
+            }
+
+        }
+
+        //按钮1 点击
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int xWidth1 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width;
+            int yHeight1 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height;
+            using (Graphics graphics = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                float dpiX = graphics.DpiX;
+                float dpiY = graphics.DpiY;
+                //public static float dpiX = dpiX;
+                //public static float dpiY = dpiY;
+                //Console.WriteLine(dpiX + "px * " + dpiY + "px");
+                string dpiX1 = Convert.ToString(dpiX);
+                label1.Text = dpiX1;
+            }
+            
+            label1.Text = ("你这电脑的分辨率是：" + "\n" + xWidth1 + "px x " + yHeight1 + "px" + "\nDPI：" + label1.Text);
+
+        }
+        //按钮2 点击
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+        //按钮3 点击
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int xWidth1 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width;
+            int yHeight1 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height;
+            using (Graphics graphics = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                float dpiX = graphics.DpiX;
+                float dpiY = graphics.DpiY;
+                //public static float dpiX = dpiX;
+                //public static float dpiY = dpiY;
+                //Console.WriteLine(dpiX + "px * " + dpiY + "px");
+                string dpiX1 = Convert.ToString(dpiX);
+                label1.Text = dpiX1;
+            }
+
+            label1.Text = ("你这电脑的分辨率是：" + "\n" + xWidth1 + "px x " + yHeight1 + "px" + "\nDPI：" + label1.Text);
+            int xWidth2 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Width;
+            int yHeight2 = System.Windows.Forms.SystemInformation.PrimaryMonitorSize.Height;
+            this.Width = xWidth1;
+            this.Height = yHeight1;
+            this.Left = 0;
+            this.Top = 0;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.TopMost = true; this.ResumeLayout(false);
+            //this.Width = xWidth2;
+            //this.Height = yHeight2;
+            //this.TransparencyKey = Color.Blue;
+            //this.BackColor = Color.Blue;
+            button1.Visible = false;
+            button3.Visible = false;
+            label1.Visible = false;
+            this.ResumeLayout(false);
+            button4.Enabled = true;
+            //pictureBox1.Image = 
+        }
+
+        //窗口按钮按下事件
+        private void win1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.KeyCode == Keys.F4)&&(e.Alt==true))  //屏蔽ALT+F4
+            {
+                e.Handled = true;
+            }
+        }
+
+        //按钮4 点击
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.TransparencyKey = BackColor;//背景透明(鼠标穿透)
+        }
+    }
+}
