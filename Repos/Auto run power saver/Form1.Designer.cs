b8ea0d8e9b5f6cba7b3a4ae720d7e990435ec6dd
yuhang0000 <104742794+yuhang0000@users.noreply.github.com>
2024-03-14 17:21:45 +0800

    Add files via upload

---
+++ Repos/Auto run power saver/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Auto_run_power_saver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel3,
+            this.toolStripStatusLabel2});
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.SizingGrip = false;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel1.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            resources.ApplyResources(this.toolStripStatusLabel1, "toolStripStatusLabel1");
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel3.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            resources.ApplyResources(this.toolStripStatusLabel3, "toolStripStatusLabel3");
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            resources.ApplyResources(this.toolStripStatusLabel2, "toolStripStatusLabel2");
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ Repos/Auto run power saver/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Auto_run_power_saver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        //鼠标监听
+        [System.Runtime.InteropServices.DllImport("user32.dll")] //导入user32.dll函数库
+        public static extern bool GetCursorPos(out System.Drawing.Point lpPoint);//获取鼠标坐标
+
+        public class mouse2333
+        {
+            public static int mpX1;
+            public static int mpY1;
+        }
+        */
+        public class freetimesss
+        {
+            public static int aaa;
+        }
+
+
+        //导入dll文件 键盘监听
+        [StructLayout(LayoutKind.Sequential)]
+        struct LASTINOUTINFO
+        {
+            //设置结构体块容量
+            [MarshalAs(UnmanagedType.U4)]
+            public int cbSize;
+            //抓获时间
+            [MarshalAs(UnmanagedType.U4)]
+            public uint dwTime;
+        }
+        [DllImport("user32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINOUTINFO Plii);//获取鼠标和键盘的空闲时间
+        public static long GetLastInputTime()
+        {
+            LASTINOUTINFO vLastInputinfo = new LASTINOUTINFO();
+            vLastInputinfo.cbSize = Marshal.SizeOf(vLastInputinfo);
+            if (!GetLastInputInfo(ref vLastInputinfo))
+            {
+                return 0;
+            }
+            else
+            {
+                long count = Environment.TickCount - (long)vLastInputinfo.dwTime;
+                //刷新周期
+                long icount = count / 50;
+                return icount;
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //打开自动运行
+            Process proc = new Process();
+            proc.StartInfo.FileName = "cmd.exe";
+            proc.StartInfo.Arguments = "/c power cfg /s 4804a71f-bacf-49e7-94c5-8d106f6b6796";
+            proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            proc.Start();
+            proc.WaitForExit();
+
+            /*
+            //一上来就给你获取初始鼠标坐标
+            System.Drawing.Point mp = new System.Drawing.Point();
+            GetCursorPos(out mp);
+            mouse2333.mpX1 = mp.X;
+            mouse2333.mpY1 = mp.Y;
+            */
+
+            //我太懒了，版本号自己打印
+            toolStripStatusLabel2.Text = "v"+System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        //循环检测
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            /*
+            //检测鼠标移动
+            System.Drawing.Point mp = new System.Drawing.Point();
+            GetCursorPos(out mp);
+            int mousex = mp.X;  //鼠标当前X坐标
+            int mousey = mp.Y;  //鼠标当前Y坐标
+            //Console.WriteLine(mp.X + "xxx" + mp.Y);
+            if (mp.X != mouse2333.mpX1 && mp.Y != mouse2333.mpY1)
+            {
+                //Console.WriteLine("我TM要886");
+                //System.Environment.Exit(0);
+            }
+            mouse2333.mpX1 = mousex;
+            mouse2333.mpY1 = mousey;
+            */
+
+            //检测键盘事件
+            /*
+            if (GetAsyncKeyState(13) !=0)
+            {
+                MessageBox.Show("你按下了回车");
+            }
+            */
+
+            //在这里！！！！！！！
+            double freeTime = GetLastInputTime();
+            /*
+            if (freeTime == 0 && freetimesss.aaa == 0)
+            {
+                freetimesss.aaa = 1;
+            }
+            else if(freeTime == 0)
+            {
+                System.Environment.Exit(0);
+            }
+            Console.WriteLine(GetLastInputTime());
+            */
+            if (freeTime == 0)
+            {
+                System.Environment.Exit(0);
+            }
+            Console.WriteLine(GetLastInputTime());
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //System.Environment.Exit(0);
+        }
+
+    }
+}
