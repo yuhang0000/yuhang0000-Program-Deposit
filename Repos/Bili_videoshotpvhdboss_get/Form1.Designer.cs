e0186a0187765b9cbc3e76fe524ce30b3a9d4aaa
yuhang0000 <104742794+yuhang0000@users.noreply.github.com>
2024-04-02 18:30:35 +0800

    Add files via upload

---
+++ Repos/Bili_videoshotpvhdboss_get/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace Bili_videoshotpvhdboss_get
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "编辑：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(477, 6);
+            this.textBox1.MaxLength = 2147483647;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(620, 411);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "就绪。";
+            this.textBox1.WordWrap = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel4,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 420);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1109, 30);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(58, 24);
+            this.toolStripStatusLabel1.Text = "就绪。";
+            // 
+            // toolStripStatusLabel4
+            // 
+            this.toolStripStatusLabel4.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.toolStripStatusLabel4.Name = "toolStripStatusLabel4";
+            this.toolStripStatusLabel4.Size = new System.Drawing.Size(22, 24);
+            this.toolStripStatusLabel4.Text = "0";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(84, 24);
+            this.toolStripStatusLabel2.Text = "错误 (000)";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(349, 24);
+            this.toolStripStatusLabel3.Text = "https://bimp.hdslb.com/videoshotpvhdboss/...";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(61, 9);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(410, 25);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "http://bimp.hdslb.com/videoshotpvhdboss/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "起始：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(61, 71);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 25);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "000000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "终止：";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(291, 71);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(180, 25);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "zzzzzz";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "数值：";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(61, 40);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(410, 25);
+            this.textBox5.TabIndex = 3;
+            this.textBox5.Text = "2333";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "周期：";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(61, 130);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(83, 25);
+            this.textBox6.TabIndex = 3;
+            this.textBox6.Text = "10";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(374, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "干它！！！";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 396);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 15);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "这里是版本号";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(150, 133);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "输出日志";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 15);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "末尾：";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(61, 102);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(410, 25);
+            this.textBox7.TabIndex = 3;
+            this.textBox7.Text = "-0001.jpg";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(318, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "继续";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(262, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "终止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 60000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1109, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1127, 497);
+            this.MinimumSize = new System.Drawing.Size(1127, 497);
+            this.Name = "Form1";
+            this.Text = "Bili Videoshotpvhdboss Get";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel4;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Repos/Bili_videoshotpvhdboss_get/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Collections;
+using static System.Windows.Forms.LinkLabel;
+using System.Media;
+
+namespace Bili_videoshotpvhdboss_get
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //哇塞，变量名可以用中文欸，太好了！
+        public static class 全局变量
+        {
+            public static String Buildtime = "2024-4-2";
+            public static String 代码串;
+            public static String 初始代码串;
+            public static int 位1;
+            public static int 位2;
+            public static int 位3;
+            public static int 位4;
+            public static int 位5;
+            public static int 位6;
+            public static int 终位1;
+            public static int 终位2;
+            public static int 终位3;
+            public static int 终位4;
+            public static int 终位5;
+            public static int 终位6;
+            public static String 数值;
+            public static String 变化码起始;
+            public static String 变化码终止;
+            public static String 变化码;
+            public static String 后缀;
+            public static String 前缀;
+            public static String 获取状态;
+            public static String 当前时间和日期;
+            public static bool Timer1的状态;
+            public static int 计数;
+            public static string 递增列表 = "0123456789abcdefghijklmnopqrstuvwxyz";
+            public static String[] 这是递增列表 = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        }
+
+        //程序刚打开就运行的地方
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //我太懒了，版本号自己打印
+            label6.Text = "v"+System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        //开始运行
+        private void button1_Click(object sender, EventArgs e)
+        {
+            全局变量.初始代码串 = textBox2.Text+textBox5.Text+"_"+textBox3.Text+textBox7.Text;
+            //Console.WriteLine(全局变量.初始代码串);
+            this.toolStripStatusLabel1.Text = "进行中";
+            this.textBox1.Text = "进行中...";
+            this.toolStripStatusLabel2.Text = "等待 (000)";
+            全局变量.数值 = textBox5.Text;
+            全局变量.前缀 = textBox2.Text;
+            全局变量.后缀 = textBox7.Text;
+            全局变量.变化码 = textBox5.Text;
+            全局变量.变化码起始 = textBox5.Text;
+            全局变量.变化码终止 = textBox4.Text;
+            全局变量.计数 = 0;
+            全局变量.代码串 = 全局变量.初始代码串;
+            全局变量.当前时间和日期 = DateTime.Now.ToString("yyyy-MM-dd") +"   "+ DateTime.Now.Hour.ToString() +"-"+ DateTime.Now.Minute.ToString() +"-"+ DateTime.Now.Second.ToString();
+            //Console.WriteLine(全局变量.当前时间和日期);
+
+            //设置循环时间
+            this.timer1.Interval = int.Parse(textBox6.Text);
+
+            //截取字符
+            全局变量.位6 = 全局变量.递增列表.IndexOf(textBox3.Text.Substring(0, 1));
+            全局变量.位5 = 全局变量.递增列表.IndexOf(textBox3.Text.Substring(1, 1));
+            全局变量.位4 = 全局变量.递增列表.IndexOf(textBox3.Text.Substring(2, 1));
+            全局变量.位3 = 全局变量.递增列表.IndexOf(textBox3.Text.Substring(3, 1));
+            全局变量.位2 = 全局变量.递增列表.IndexOf(textBox3.Text.Substring(4, 1));
+            全局变量.位1 = 全局变量.递增列表.IndexOf(textBox3.Text.Substring(5, 1));
+            //全局变量.终位1 = int.Parse(textBox4.Text.Substring(0, 1));
+            //全局变量.终位2 = int.Parse(textBox4.Text.Substring(1, 1));
+            //全局变量.终位3 = int.Parse(textBox4.Text.Substring(2, 1));
+            //全局变量.终位4 = int.Parse(textBox4.Text.Substring(3, 1));
+            //全局变量.终位5 = int.Parse(textBox4.Text.Substring(4, 1));
+            //全局变量.终位6 = int.Parse(textBox4.Text.Substring(5, 1));
+
+
+            //Console.WriteLine(全局变量.递增列表.IndexOf("o"));
+
+            /*
+            Console.WriteLine(全局变量.位1);
+            Console.WriteLine(全局变量.位2);
+            Console.WriteLine(全局变量.位3);
+            Console.WriteLine(全局变量.位4);
+            Console.WriteLine(全局变量.位5);
+            Console.WriteLine(全局变量.位6);
+            Console.WriteLine(全局变量.终位1);
+            Console.WriteLine(全局变量.终位2);
+            Console.WriteLine(全局变量.终位3);
+            Console.WriteLine(全局变量.终位4);
+            Console.WriteLine(全局变量.终位5);
+            Console.WriteLine(全局变量.终位6);
+            */
+
+            /*
+            var client = new HttpClient();
+            var 输出 = await client.GetAsync(全局变量.初始代码串);
+            //var 输出 = await client.GetAsync("https://bimp.hdslb.com/videoshotpvhdboss/1482069818_vifmja-0001.jpg");
+            //Console.WriteLine(输出);
+            this.textBox1.Text = 输出.ToString();
+            //状态码
+            //Console.WriteLine((int)输出.StatusCode);
+            if (((int)输出.StatusCode).ToString() == "200")
+            {
+                全局变量.获取状态 = "成功";
+            }
+            else if (((int)输出.StatusCode).ToString() == "404")
+            {
+                全局变量.获取状态 = "失败";
+            }
+            this.toolStripStatusLabel2.Text = 全局变量.获取状态 +  " (" + ((int)输出.StatusCode).ToString() + ")";
+            */
+
+            this.timer1.Enabled = true;
+            this.timer2.Enabled = true;
+            this.button3.Enabled = true;
+            this.button2.Enabled = false;
+            全局变量.Timer1的状态 = true;
+        }
+
+        
+        
+
+        class test
+        {
+            private static readonly HttpClient client = new HttpClient();
+            static async Task Main(string[] args)
+            {
+                //发送Get请求
+                var responseString = await client.GetStringAsync("https://bimp.hdslb.com/videoshotpvhdboss/1482069818_vifmja-0001.jpg");
+                Console.WriteLine(responseString);
+            }
+        }
+
+
+        //循环器
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            //计数
+            全局变量.计数 = 全局变量.计数 + 1;
+            this.toolStripStatusLabel4.Text = 全局变量.计数.ToString();
+            //int abc = 12;
+            //this.toolStripStatusLabel4.Text = 全局变量.这是递增列表[abc].ToString();
+            //this.toolStripStatusLabel3.Text = 全局变量.代码串;
+
+            //打印到状态栏
+            this.toolStripStatusLabel3.Text = 全局变量.前缀 + 全局变量.数值 +"_"+ 全局变量.这是递增列表[全局变量.位6].ToString() + 全局变量.这是递增列表[全局变量.位5].ToString() + 全局变量.这是递增列表[全局变量.位4].ToString() + 全局变量.这是递增列表[全局变量.位3].ToString() + 全局变量.这是递增列表[全局变量.位2].ToString() + 全局变量.这是递增列表[全局变量.位1].ToString() + 全局变量.后缀;
+
+
+            //网络爬取
+            timer1.Stop();
+            var client = new HttpClient();
+            //var 输出 = await client.GetAsync(全局变量.初始代码串);
+            var 输出 = await client.GetAsync(this.toolStripStatusLabel3.Text);
+            //Console.WriteLine(输出);
+            this.textBox1.Text = 输出.ToString();
+            //状态码
+            //Console.WriteLine((int)输出.StatusCode);
+            if (((int)输出.StatusCode).ToString() == "200")
+            {
+                全局变量.获取状态 = "成功";
+
+                this.button2.Enabled = true;
+                this.button3.Enabled = false;
+                this.toolStripStatusLabel1.Text = "成功";
+
+                SystemSounds.Beep.Play();
+                if (MessageBox.Show(this.toolStripStatusLabel3.Text, "成功    q(≧▽≦q)",MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    //复制到剪切板
+                    Clipboard.SetDataObject(this.toolStripStatusLabel3.Text);
+                }
+            }
+            else if (((int)输出.StatusCode).ToString() == "404")
+            {
+                全局变量.获取状态 = "失败";
+                if (全局变量.Timer1的状态 != false)
+                {
+                    timer1.Start();
+                }
+            }
+            this.toolStripStatusLabel2.Text = 全局变量.获取状态 +  " (" + ((int)输出.StatusCode).ToString() + ")";
+            //内存回收
+            client.Dispose();
+            输出.Dispose();
+            输出 = null;
+            client = null;  
+            GC.Collect();
+            
+
+
+            //网址遍历
+            全局变量.位1 = 全局变量.位1 + 1;
+            if (全局变量.位1 > 35)
+            {
+                全局变量.位1 = 0;
+                全局变量.位2 = 全局变量.位2 + 1;
+            }
+            if (全局变量.位2 > 35)
+            {
+                全局变量.位2 = 0;
+                全局变量.位3 = 全局变量.位3 + 1;
+            }
+            if (全局变量.位3 > 35)
+            {
+                全局变量.位3 = 0;
+                全局变量.位4 = 全局变量.位4 + 1;
+            }
+            if (全局变量.位4 > 35)
+            {
+                全局变量.位4 = 0;
+                全局变量.位5 = 全局变量.位5 + 1;
+            }
+            if (全局变量.位5 > 35)
+            {
+                全局变量.位5 = 0;
+                全局变量.位6 = 全局变量.位6 + 1;
+            }
+            if (全局变量.位6 > 35)
+            {
+                全局变量.位6 = 0;
+
+                //this.timer1.Enabled = false;
+                全局变量.Timer1的状态 = false;
+                timer1.Stop();
+                timer2.Stop();
+                this.button2.Enabled = false;
+                this.button3.Enabled = false;
+                this.toolStripStatusLabel1.Text = "失败";
+                System.Media.SystemSounds.Hand.Play();
+                MessageBox.Show("我尽力了QwQ", "失败    ╥﹏╥...");
+            }
+            
+        }
+
+        //继续
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //this.timer1.Enabled = true;
+            this.toolStripStatusLabel1.Text = "进行中...";
+            全局变量.Timer1的状态 = true;
+            timer1.Start(); 
+            timer2.Start();
+            this.button3.Enabled = true;
+            this.button2.Enabled = false;
+        }
+        //暂停
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //this.timer1.Enabled = false;
+            this.toolStripStatusLabel1.Text = "暂停";
+            全局变量.Timer1的状态 = false;
+            timer1.Stop();  
+            timer2.Stop();
+            this.button2.Enabled = true;
+            this.button3.Enabled = false;
+        }
+
+        //写日志
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                System.IO.File.WriteAllLines(@".\Logs\"+全局变量.当前时间和日期+".txt", toolStripStatusLabel3.Text.Split());
+            }
+        }
+
+        //关于
+        private void label6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bili_videoshotpvhdboss_get\nBy：yuhang0000\nBuild Time：" + 全局变量.Buildtime + "\n版本号：" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() , "关于");
+        }
+    }
+}
