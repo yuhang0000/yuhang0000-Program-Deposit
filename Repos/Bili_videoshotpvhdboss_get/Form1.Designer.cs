b6c545218def1dbd65eb7b7374d289efdd32a9d0
yuhang0000 <104742794+yuhang0000@users.noreply.github.com>
2024-04-03 19:48:01 +0800

    Add files via upload

--- Repos/Bili_videoshotpvhdboss_get/Form1.Designer.cs
+++ Repos/Bili_videoshotpvhdboss_get/Form1.Designer.cs
@@ -270,7 +270,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(50, 23);
             this.button3.TabIndex = 6;
-            this.button3.Text = "终止";
+            this.button3.Text = "暂停";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
