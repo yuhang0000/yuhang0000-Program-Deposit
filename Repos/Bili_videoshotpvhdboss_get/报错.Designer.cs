59b76a5ffc6b93ddf80ea7cb8bc125893356ae7c
yuhang0000 <104742794+yuhang0000@users.noreply.github.com>
2024-11-18 20:09:11 +0800

    Add files via upload

--- Repos/Bili_videoshotpvhdboss_get/报错.Designer.cs
+++ Repos/Bili_videoshotpvhdboss_get/报错.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.listView1 = new System.Windows.Forms.ListView();
-            this.button1 = new System.Windows.Forms.Button();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listView1
@@ -52,6 +52,20 @@
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "序列";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "时间";
+            this.columnHeader2.Width = 170;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "详细信息";
+            this.columnHeader3.Width = 787;
+            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 334);
@@ -62,20 +76,6 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "序列";
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "时间";
-            this.columnHeader2.Width = 160;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "详细信息";
-            this.columnHeader3.Width = 797;
             // 
             // 报错
             // 
