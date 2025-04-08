c1d5090478e5d8eb4229a731e498e55aade4a352
yuhang0000 <13975138250@163.com>
2025-04-08 13:06:04 +0800

    Repos\Close The Display # 新增, 儅關閉筆電蓋子時自動鎖定賬戶.

---
+++ Repos/Close The Display/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Close_The_Display
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(109, 28);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(210, 24);
+            this.toolStripMenuItem1.Text = "退出";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Close The Display";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(0, 0);
+            this.ControlBox = false;
+            this.Enabled = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(-1, -1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Close The Display";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Activated);
+            this.Enter += new System.EventHandler(this.Form1_Activated);
+            this.Leave += new System.EventHandler(this.Form1_Activated);
+            this.Validated += new System.EventHandler(this.Form1_Activated);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Repos/Close The Display/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace Close_The_Display
+{
+    public partial class Form1 : Form
+    {
+
+        protected override void WndProc(ref Message m)
+        {
+            //状态机
+            //DateTime now = DateTime.Now;
+            //Console.WriteLine(now + "| " + m.Msg.ToString());
+            //this.textBox1.AppendText(m.Msg + "\r\n");
+            switch (m.Msg)
+            {
+                case 0x218:
+                    Console.WriteLine("接收消息: " + m.WParam.ToString());
+                    if(m.WParam.ToInt32() == 0x8013) //已收到电源设置更改事件。
+                    {
+                        /*int size = Marshal.SizeOf(m.LParam); //获取结构体大小
+                        IntPtr freesize = Marshal.AllocHGlobal(size); //分配相等的内存池
+                        Marshal.StructureToPtr(m.LParam,freesize,false); //ctrl + C/V
+                        byte[] buffer = new byte[size];
+                        Marshal.Copy(freesize,buffer,0,size); //新内存池，比特数组，不知道是啥，大小
+                        Marshal.FreeHGlobal(freesize);
+                        String buffers = "";
+                        foreach(byte b in buffer)
+                        {
+                            buffers = buffers + b.ToString() + " ";
+                        }
+                        Console.WriteLine(buffers);
+                        if (buffer[0].ToString() == "32")
+                        {
+                            Process.Start("Rundll32.exe user32.dll,LockWorkStation");
+                        }*/
+                        MyStruct myStruct = (MyStruct)Marshal.PtrToStructure(m.LParam,typeof(MyStruct));
+                        Guid guid = Guid.Parse("BA3E0F4D-B817-4094-A2D1-D56379E6A0F3");
+                        if (myStruct.PowerSetting == guid)
+                        {
+                            if (sss.wait == 1)
+                            {
+                                sss.wait = 0;
+                            }
+                            else
+                            {
+                                Process.Start("Rundll32.exe", "user32.dll,LockWorkStation");
+                            }
+                        }
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        //创建结构体
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MyStruct
+        {
+            public Guid PowerSetting;
+            public uint DataLength;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)] //固定data数量为 1 个
+            public byte[] Data;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)] //注册
+        public static extern IntPtr RegisterPowerSettingNotification(IntPtr hRecipient,
+            [MarshalAs(UnmanagedType.LPStruct)] Guid PowerSettingGuid, uint Flags);
+        [DllImport("user32.dll", SetLastError = true)] //注销
+        public static extern void UnregisterPowerSettingNotification(IntPtr Handle);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        public static class sss
+        {
+            public static int wait = 1;
+            public static IntPtr reback;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Guid guid = Guid.Parse("BA3E0F4D-B817-4094-A2D1-D56379E6A0F3");
+            try
+            {
+                sss.reback = RegisterPowerSettingNotification(this.Handle, guid, 0);
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message,"Oops! ");
+                UnregisterPowerSettingNotification(sss.reback);
+            }
+            int a = GetWindowLong(this.Handle, -20);
+            SetWindowLong(this.Handle, -20, a | 0x00000080); //在 alt + Tab 上颖仓
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterPowerSettingNotification(sss.reback);
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sss.wait = 0;
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
