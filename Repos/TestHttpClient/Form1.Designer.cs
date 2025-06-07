6afc9b568338aeff70732a723cc96e28ff1819ee
yuhang0000 <13975138250@163.com>
2025-06-07 16:56:04 +0800

    Add: 新添網路延遲測試軟體.

---
+++ Repos/TestHttpClient/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace TestHttpClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(1041, 499);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1017, 381);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(1016, 25);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "https://api.bilibili.com/x/v3/fav/resource/list?media_id=";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 44);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1016, 25);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "&pn=1&ps=1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(13, 75);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(400, 25);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.Text = "10000";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(419, 75);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(400, 25);
+            this.textBox5.TabIndex = 2;
+            this.textBox5.Text = "100";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(825, 77);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 19);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "允许重试";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(937, 462);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "重置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 499);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "TestHttpClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Repos/TestHttpClient/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestHttpClient
+{
+    public partial class Form1 : Form
+    {
+        public int num = 100000;
+        public int max = 100;
+        public string urlleft = "";
+        public string urlright = "";
+        public bool retry = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        async Task runs(string url)
+        {
+            try
+            {
+                int lll = await http(url);
+                num++;
+                update(gettext() + "\t成功\t" + lll);
+
+            }
+            catch {
+                if (retry == true)
+                {
+                    await runs(url);
+                }
+                else
+                {
+                    num++;
+                    update(gettext() + "\t失败\t" + "0");
+                }
+                return;
+            }
+        }
+
+        async void run()
+        {
+            DateTime start = DateTime.Now;
+
+            //string temp1 = urlleft + num.ToString() + urlright;
+            //int tab = temp1.Length / 8 / 2;
+
+            update("开始于: " + start.ToString() + "\tURL\t\t\t\t\t\t状态\t长度\t时间");
+            for (int i = 0; i < max; i++)
+            {
+                DateTime ss = DateTime.Now;
+                string url = urlleft + num + urlright;
+                update(gettext() + "\r\n" + url);
+                await runs(url);
+                DateTime ee = DateTime.Now;
+                TimeSpan ssee = ee - ss;
+                update(gettext() + "\t" + ssee.TotalSeconds + "s");
+
+                title(i);
+            }
+            DateTime end = DateTime.Now;
+            update(gettext() + "\r\n开始于: " + start.ToString() + "\t结束于: " + end.ToString());
+
+            TimeSpan ts = end - start;
+            update(gettext() + "\t花费: " + ts.TotalSeconds + "s");
+
+            if (this.IsHandleCreated == false)
+            {
+                this.button2.Visible = true;
+            }
+            else
+            {
+                this.Invoke(new MethodInvoker( () =>
+                {
+                    this.button2.Visible = true;
+                }));
+            }
+        }
+
+        public void update(string ttt)
+        {
+            if (this.IsHandleCreated == false)
+            {
+                this.textBox1.Text = ttt;
+                this.textBox1.SelectionStart = this.textBox1.TextLength;
+                this.textBox1.ScrollToCaret();
+            }
+            else
+            {
+                this.Invoke(new MethodInvoker( () =>
+                {
+                    this.textBox1.Text = ttt;
+                    this.textBox1.SelectionStart = this.textBox1.TextLength;
+                    this.textBox1.ScrollToCaret();
+                }));
+            }
+        }
+
+        public void title(int tt)
+        {
+            int ttt = tt + 1;
+            if (this.IsHandleCreated == false)
+            {
+                this.Text = "TestHttpClient (" + ttt.ToString() + "\\" + max.ToString() + ")";
+            }
+            else
+            {
+                this.Invoke(new MethodInvoker( () =>
+                {
+                    this.Text = "TestHttpClient (" + ttt.ToString() + "\\" + max.ToString() + ")";
+                }));
+            }
+        }
+
+        public string gettext()
+        {
+            string ttt = "";
+            if (this.IsHandleCreated == false)
+            {
+                return this.textBox1.Text;
+            }
+            else
+            {
+                this.Invoke(new MethodInvoker( () =>
+                {
+                    ttt = this.textBox1.Text;
+                }));
+                return ttt;
+            }
+        }
+
+        async Task<int> http(string url)
+        {
+            HttpClient client = new HttpClient();
+            var res = await client.GetAsync(url);
+            res.EnsureSuccessStatusCode();
+            string data = await res.Content.ReadAsStringAsync();
+            client.Dispose();
+            res.Dispose();
+            return data.Length;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Visible = true;
+            this.textBox2.Visible = false;
+            this.textBox3.Visible = false;
+            this.textBox4.Visible = false;
+            this.textBox5.Visible = false;
+            this.button1.Visible = false;
+            this.checkBox1.Visible = false;
+
+            retry = this.checkBox1.Checked;
+            urlleft = this.textBox2.Text;
+            urlright = this.textBox3.Text;
+            num = int.Parse(this.textBox4.Text);
+            max = int.Parse(this.textBox5.Text);
+            this.Text = "TextHttpClient " + "(0\\" + max + ")";
+            run();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "";
+            this.textBox1.Visible = false;
+            this.textBox2.Visible = true;
+            this.textBox3.Visible = true;
+            this.textBox4.Visible = true;
+            this.textBox5.Visible = true;
+            this.button1.Visible = true;
+            this.button2.Visible = false;
+            this.checkBox1.Visible = true;
+        }
+    }
+}
