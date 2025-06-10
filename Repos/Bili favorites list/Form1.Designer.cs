6b9fa65b00e8af0a8be5552d71b4df11032300d1
yuhang0000 <13975138250@163.com>
2025-06-10 23:17:10 +0800

    Update: 給隊列設置最大值.

--- Repos/Bili favorites list/Form1.Designer.cs
+++ Repos/Bili favorites list/Form1.Designer.cs
@@ -316,6 +316,11 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(59, 192);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
             this.numericUpDown1.Minimum = new decimal(new int[] {
             1,
             0,
