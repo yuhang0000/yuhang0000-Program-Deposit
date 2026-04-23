bcb274ab20e9c3995b79e480b747fd477aff8243
yuhang0000 <13975138250@163.com>
2026-04-23 12:28:11 +0800

    Feat: # 新增 FolderIconFix 用於讓資料夾的圖像路徑設定爲相對路徑.

---
+++ Repos/FolderIconFIx/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace FolderIconFIx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnFolder = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusTextVersion = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StatusTextNum = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "路径: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(732, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // BtnFolder
+            // 
+            this.BtnFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnFolder.Location = new System.Drawing.Point(800, 5);
+            this.BtnFolder.Name = "BtnFolder";
+            this.BtnFolder.Size = new System.Drawing.Size(32, 23);
+            this.BtnFolder.TabIndex = 0;
+            this.BtnFolder.Text = "..";
+            this.BtnFolder.UseVisualStyleBackColor = true;
+            this.BtnFolder.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnExit.Location = new System.Drawing.Point(906, 5);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(62, 23);
+            this.BtnExit.TabIndex = 3;
+            this.BtnExit.Text = "退出";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            this.BtnExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnStart.Location = new System.Drawing.Point(838, 5);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(62, 23);
+            this.BtnStart.TabIndex = 2;
+            this.BtnStart.Text = "搜索";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusTextVersion,
+            this.StatusTextNum});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 420);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(980, 30);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusTextVersion
+            // 
+            this.StatusTextVersion.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.StatusTextVersion.Name = "StatusTextVersion";
+            this.StatusTextVersion.Size = new System.Drawing.Size(69, 24);
+            this.StatusTextVersion.Text = "v0.0.0.1";
+            this.StatusTextVersion.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // StatusTextNum
+            // 
+            this.StatusTextNum.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.StatusTextNum.Name = "StatusTextNum";
+            this.StatusTextNum.Size = new System.Drawing.Size(37, 24);
+            this.StatusTextNum.Text = "0/0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox2.Location = new System.Drawing.Point(0, 37);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(980, 383);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnFolder);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FolderIconFIx";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnFolder;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusTextVersion;
+        private System.Windows.Forms.ToolStripStatusLabel StatusTextNum;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Repos/FolderIconFIx/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderIconFIx
+{
+    public partial class Form1 : Form
+    {
+        //构造函数
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = Application.ProductName;
+            this.StatusTextVersion.Text = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+        }
+
+        /// <summary>
+        /// 当前模式, false: 搜索; true: 执行
+        /// </summary>
+        public bool ModeCode = false;
+        /// <summary>
+        /// 运行状态
+        /// </summary>
+        public bool StatusCode = false;
+        /// <summary>
+        /// 暂存 Desktop.ini 路径
+        /// </summary>
+        public List<string> DesktopPaths = new List<string>();   
+
+        //关于
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+            String 康派利 = Application.CompanyName;
+            String 奈姆 = Assembly.GetExecutingAssembly().GetName().Name;
+            String 版本 = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            DateTime 编译时间 = File.GetLastWriteTime(typeof(Form1).Assembly.Location);
+            String text = 奈姆 + "\r\nBy: " + 康派利 +"\r\nBuild Time: " + 编译时间 + "\r\n版本号: " + 版本;
+            MessageBox.Show(text, "关于");
+        }
+
+        //退出
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //打开资料夹选择器
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "选定指定资料夹. ";
+            dialog.SelectedPath = this.textBox1.Text;
+            if(dialog.ShowDialog() == DialogResult.OK && dialog.SelectedPath != null && dialog.SelectedPath.Length > 0)
+            {
+                this.textBox1.Text = dialog.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// 获取当前时间戳
+        /// </summary>
+        /// <param name="target">给定时间对象, 缺省时取当前时间</param>
+        /// <returns>long: 时间戳</returns>
+        public static long GetTimeStamp(DateTime? target = null)
+        {
+            DateTime now;
+            if (target == null)
+            {
+                now = DateTime.Now;
+            }
+            else
+            {
+                now = (DateTime)target;
+            }
+            DateTime old = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            long timestamp = (long)(now - old).TotalSeconds;
+
+            return timestamp;
+        }
+
+        // 搜索 & 开始 & 终止
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(this.StatusCode == false)
+            {
+                this.StatusCode = true;
+                this.BtnStart.Enabled = false;
+                //搜索
+                if (this.ModeCode == false)
+                {
+                    Sreach();
+                }
+                //开始
+                else
+                {
+                    Start();
+                }
+            }
+            //终止
+            else
+            {
+                this.StatusCode = false;
+            }
+        }
+
+        /// <summary>
+        /// 搜索
+        /// </summary>
+        public void Sreach()
+        {
+            if(this.textBox1.Text.Length == 0)
+            {
+                this.StatusCode = false;
+                this.BtnStart.Enabled = true;
+                SystemSounds.Hand.Play();
+                return;
+            }
+            string[] Folders;
+            try
+            {
+                long starttime = GetTimeStamp();
+                Folders = Directory.GetDirectories(this.textBox1.Text);
+                this.textBox2.Clear();
+                this.DesktopPaths.Clear();
+                foreach(string folder in Folders)
+                {
+                    if (File.Exists(folder + "\\Desktop.ini") == true)
+                    {
+                        this.DesktopPaths.Add(folder + "\\Desktop.ini");
+                        this.textBox2.AppendText(folder + "\\Desktop.ini" + "\r\n");
+                    }
+                }
+                long lasttime = GetTimeStamp();
+                this.textBox2.AppendText("搜索用时: " + (lasttime - starttime).ToString() + "s");
+                this.StatusTextNum.Text = "0/" + this.DesktopPaths.Count.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("遍历文件夹时出现了问题, 原因是: \r\n" + ex.ToString(),"Oops",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                this.StatusCode = false;
+                this.BtnStart.Enabled = true;
+                SystemSounds.Hand.Play();
+            }
+            this.StatusCode = false;
+            this.ModeCode = true;
+            this.BtnStart.Text = "开始";
+            this.BtnStart.Enabled = true;
+        }
+        /// <summary>
+        /// 开始
+        /// </summary>
+        public void Start()
+        {
+            this.StatusCode = true;
+            this.BtnStart.Enabled = false;
+
+            int finishnum = 0;
+            foreach(string folder in this.DesktopPaths)
+            {
+                try
+                {
+                    string[] texts = File.ReadAllLines(folder);
+                    //遍历每行文本
+                    string iconpath = ""; //图标路径
+                    string tmp1 = "IconResource=";
+                    int index = 0; //暂存索引
+                    for(int i = 0; i < texts.Length; i++)
+                    {
+                        string text = texts[i];
+                        if(text.IndexOf(tmp1)  != -1)
+                        {
+                            iconpath = text.Substring(tmp1.Length, text.IndexOf(","));
+                            index = i;
+                        }
+                    }
+                    if(iconpath != "")
+                    {
+                        string[] tmp2; //吧路径切成腻子, 图标路径
+                        string[] tmp3; //吧路径切成腻子, 当前文件夹路径
+                        tmp2 = iconpath.Split('\\');
+                        tmp3 = folder.Split('\\');
+                        if (tmp2[0] !=  tmp3[0] || tmp2[1] == "." || tmp2[1] == "..") //不在同个盘符或者已经设置了相对路径, 就跳过
+                        {
+                            //跳过
+                            //continue;
+                        }
+                        else
+                        {
+                            int lastindex = tmp3.Length - 2; //遍历每个腻子, 这个记录, 当遇到比较不同文件夹名称时, 最后的索引, 默认是文件夹所在路径, 去除 Desktop.ini 和盘符的数组长度
+                            for(int i = 1; i < tmp3.Length - 1;i++) //跳过第一个和最后一个
+                            {
+                                if (tmp2[i] == tmp3[i])
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    lastindex = i - 1;
+                                }
+                            }
+                            string newiconpath = "";
+                            //往前
+                            if(lastindex < tmp2.Length - 2)
+                            {
+                                for(int i = lastindex; i < tmp2.Length - 1; i++)
+                                {
+                                    newiconpath = "..\\" +  newiconpath + tmp2[i];
+                                }
+                            }
+                            //往后
+                            else
+                            {
+                                newiconpath = ".\\";
+                            }
+                        }
+                    }
+
+                    finishnum++;
+                    this.StatusTextNum.Text = finishnum.ToString() +" /" + this.DesktopPaths.Count.ToString();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("修补文件时出现了问题, 原因是: \r\n" + ex.ToString(), "Oops", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.StatusCode = false;
+                    this.BtnStart.Enabled = true;
+                    SystemSounds.Hand.Play();
+                }
+            }
+            this.StatusCode = false;
+            this.BtnStart.Enabled = true;
+        }
+
+        //路径文本礦变更时
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.ModeCode = false;
+            this.BtnStart.Text = "搜索";
+        }
+    }
+}
